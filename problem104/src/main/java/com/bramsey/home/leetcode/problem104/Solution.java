1ac34b3381a453e2ca586b6ff86d41f5849d253f
Ben Ramsey <benramsey25@gmail.com>
2021-09-11 15:12:29 +0930

    Add solution to problem 104

---
+++ problem104/src/main/java/com/bramsey/home/leetcode/problem104/Solution.java
@@ -0,0 +1,18 @@
+package com.bramsey.home.leetcode.problem104;
+
+import com.bramsey.home.leetcode.common.TreeNode;
+
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
